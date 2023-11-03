3961dbac1673d93bdb59607dd1901c679f147708
Akanksha Gurunath Halle <113449675+Akanksha7744@users.noreply.github.com>
2023-11-03 19:21:07 +0530

    Add files via upload

---
+++ LCM.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+ 
+public class LCM  {
+    public static void main(String[] args)
+    {
+        int a = 15, b = 25;
+        int ans = (a > b) ? a : b;
+ 
+        while (true) {
+            if (ans % a == 0 && ans % b == 0)
+                break;
+            ans++;
+        }
+ 
+        System.out.println("LCM of " + a + " and " + b
+                           + " : " + ans);
+    }
+}
